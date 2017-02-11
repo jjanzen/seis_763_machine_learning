f146e3d15b24dc7156b20edfaef77d144f1f1dd9
josh <joshjanzen@gmail.com>
2017-02-10 19:33:37 -0600

    cleaned up formulas

--- r_basics.R
+++ r_basics.R
@@ -1,70 +1,82 @@
+# r basics (similar to matlab for lecture 2)
+# seis763-02, Spring 2017, Prof Lai
+# by josh janzen
+# there is more than 1 way to do these operations (and I'm not saying this is the best way)
+
+# assign variables
 a <- 5
 b <- 3
+
+# addition
 a + b
+
+# infinity
 c = Inf
 
-occur <- matrix(c(1,1,1),ncol=3,byrow=TRUE)
-occur
+# 3x4 matrix of ones
+matrix(c(1),nrow=3,ncol=4)
 
-d = random(3,4)
-e = ones(3,4)
-ones(3,4) *9
+# numeric vector
+vector(mode="numeric", length=3)
 
-x <- vector(mode="numeric", length=3)
-x
-seq1 <- seq(1:6)
-mat1 <- matrix(seq1, nrow=2)
-mat1
+# matrix 1-6 (filled by column)
+matrix(1:6, nrow=2)
 
-s <- matrix(1:6, ncol=3, byrow=TRUE)
+# matrix 1-6 (filled by row)
+matrix(1:6, nrow=2, ncol=3, byrow=T)
+
+# stacking 1s on top of matrix
+s <- matrix(1:6, nrow=2, ncol=3, byrow=TRUE)
 st <- rbind(c(1,1,1),s)
 st
 
+# stacking 7-9 on bottom of matrix
 stu <- rbind(st,(7:9))
 stu
 
+# 2nd and 3rd rows, 1st and 2nd columns
 stu[2:3,1:2]
 
+# 3rd and 4th rows, 2nd and 3rd columns
 stu[3:4,2:3]
 
+# get last two rows and last two columns
 stu[(nrow(stu)-1):nrow(stu),(ncol(stu)-1):ncol(stu)]
+
+# element 3 down and 2 over 
 stu[3,2]
 
-stu[3,] < NA
+# setting 3rd row as NA
+stu[3,] <- NA
+stu
 
-a <- matrix(c(1,1,1,2,3,4), nrow=2, byrow=T)
-a
+# create 2 x 3 matrix, filled by specified vector
+z <- matrix(c(1,1,1,2,3,4), nrow=2, byrow=T)
 
-t <- matrix(c(1,5),ncol=1)
-t
+# dimensions of t
+dim(z)
 
-dim(a)
-dim(t(t))
+# transpose matrix
+(t(z))
 
-a *  (t)
+# multiples variable 5 by matrix
+a * z
 
-# 3 columns 20 rows from normal dist
-replicate(3, rnorm(20)) 
+# 3 columns 10 rows from normal dist
+replicate(3, rnorm(10)) 
 
-set.seed(133)
-
-b <- (1:10)
-
+# for loop example 1
 for (year in c(2010,2011,2012,2013,2014,2015)){
   print(paste("The year is", year))
 }
 
+# for loop example 2
 years <- c(2010,2011,2012,2013,2014,2015)
 for (year in 1:length(years)){
   print(paste("The year is", years[year]))
 }
 
-D <- matrix(c(2,-2,1,2,3,1),2,3)
-A <- matrix(c(2,3,-2,1,2,2),3,2)
-A %*% D
-
+# matrix mutiplication
 theta <- matrix(c(0,4), ncol=1)
-theta
 X <- matrix(c(1,1,1,1,2,3), ncol=3, nrow=2, byrow=T)
-X
 t(theta) %*% X
